ce2ec52c39bb0a1db4dbe9fc5b2f4574f3013638
Koushik Jana <128731535+KOUSHIKJANA007@users.noreply.github.com>
2024-08-07 22:13:10 +0530

    Update PostList.jsx
    de

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -5,7 +5,25 @@ import WelcomeMessage from './WelcomeMessage';
 import LoadingSpinner from './LoadingSpinner';
 
 const PostList = () => {
-  const { postList, addInitialPosts } = useContext(PostListData);
+  const DEFAULT_POST_LIST = [
+    {
+        id: '1',
+        title: 'going to mumbai',
+        body: 'Hi friends i am going to mumbai',
+        reactions: '1',
+        userId: 'user-9',
+        tags: ["mumbai", "vacation", "enjoy"]
+    },
+    {
+        id: '2',
+        title: 'pass ho bhai',
+        body: 'Hi friends i am passed',
+        reactions: '12',
+        userId: 'user-12',
+        tags: ["passed", "exam", "enjoy"]
+    }
+]
+  // const { postList, addInitialPosts } = useContext(PostListData);
   const [fetching,setFetching]=useState(false);
   useEffect(() => {
     setFetching(true);
@@ -15,7 +33,7 @@ const PostList = () => {
     fetch('https://dummyjson.com/posts',{signal})
       .then((res) => res.json())
       .then((data) => {
-        addInitialPosts(data.posts);
+        // addInitialPosts(data.posts);
         setFetching(false);
       });
       return ()=>{
@@ -29,7 +47,7 @@ console.log("post list is",postList);
     {fetching && <LoadingSpinner></LoadingSpinner>}
       { !fetching && postList.length === 0 && <WelcomeMessage></WelcomeMessage>}
       {
-        !fetching && postList.map((post) => (
+        !fetching && DEFAULT_POST_LIST.map((post) => (
           <Post key={post.id} post={post}></Post>
         ))}
     </>
